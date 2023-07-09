6c0aae2e73dff80b8c96c96d853584f93156e11e
jordanful <j.fulghum@gmail.com>
2023-07-09 07:54:29 -0400

    COCO: Fix docker issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Wait for the DB to be ready
 dockerize -wait tcp://db:5432 -timeout 1m
-
-# bin/rails db:seed
-# bin/rails db:migrate
 bin/rails db:setup
-
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
